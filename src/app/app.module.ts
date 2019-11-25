9d075f95d5ca818863768738eb8524736ff2a9c8
daryaU <daryashavzova@gmail.com>
2019-11-25 23:26:16 +0300

    add all project without form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { SignComponent } from './sign/sign.component';
+import { FooterComponent } from './footer/footer.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'sign',
+    component: SignComponent
+  },
+  { path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SignComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
